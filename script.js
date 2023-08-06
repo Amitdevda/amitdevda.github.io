5035eb698b49d0a68f1f7d9b91b7d13fbc46bb22
Amitdevda <akd9t7@gmail.com>
2023-08-06 13:35:06 +0530

    Portfolio#

---
+++ script.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Full Stack Web Developer", "Backend Developer", "MERN Stack Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Full Stack Web Developer", "Backend Developer", "MERN Stack Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    // Resume
+    let openResume = document.getElementById("resume-button-1");
+    
+    openResume.addEventListener("click", () => {
+    window.open('https://drive.google.com/file/d/1E8fBkbU0QvY5G38ERLlsZFcUmky0bFq8/view?usp=sharing', '_blank'); 
+   })
+let openResume2 = document.getElementById("resume-button-2");
+ 
+       openResume2.addEventListener("click", () => {
+       window.open('https://drive.google.com/file/d/1E8fBkbU0QvY5G38ERLlsZFcUmky0bFq8/view?usp=sharing', '_blank'); 
+      })
+
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
